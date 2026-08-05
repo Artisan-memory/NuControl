7e432f799c8bb5cebe7917ab47756db38b3c7776
Artisan-Memory <dataroofer@gmail.com>
2026-08-05 06:16:04 +0300

    feat: register the i18n middleware
    It has to run as an outer middleware so the auth reply and every handler already have a locale.

--- src/bot/middlewares/__init__.py
+++ src/bot/middlewares/__init__.py
@@ -1,18 +1,16 @@
 from aiogram import Dispatcher
 from aiogram.utils.callback_answer import CallbackAnswerMiddleware
 
-# from src.bot.core.loader import i18n as _i18n
-
 
 def register_middlewares(dp: Dispatcher) -> None:
     from .auth import AuthMiddleware
-#   from .i18n import ACLMiddleware
-    from .logging import LoggingMiddleware  
+    from .logging import LoggingMiddleware
+    from .i18n import ConfigI18nMiddleware
+    from src.bot.core.loader import i18n
 
     dp.update.outer_middleware(LoggingMiddleware())
 
+    ConfigI18nMiddleware(i18n=i18n).setup(dp)
+
     dp.message.middleware(AuthMiddleware())
-
-    # ACLMiddleware(i18n=_i18n).setup(dp)
-
     dp.callback_query.middleware(CallbackAnswerMiddleware())
